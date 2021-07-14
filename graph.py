84ac4512f590a56dc634e1b0bf18b33c773a5e27
Vaibhav Bangia <vaibhavbangia@gmail.com>
2021-07-14 14:10:47 +0530

    /

--- graph.py
+++ graph.py
@@ -26,40 +26,35 @@ import numpy as np
 # import numpy as np
 import percentages
 
-# labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# men_means = [20, 34, 30, 35, 27, 20, 34, 30, 35, 27, 56]
-# women_means = [25, 32, 34, 20, 25, 20, 34, 30, 35, 27, 75]
+men_means = [20, 34, 30, 35, 27, 20, 34, 30, 35, 27, 56]
+women_means = [25, 32, 34, 20, 25, 20, 34, 30, 35, 27, 75]
 
-# x = np.arange(len(labels))  # the label locations
-# width = 0.35  # the width of the bars
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
 
-# fig, ax = plt.subplots()
-# rects1 = ax.bar(x - width/2, percentages.EE2K18sem3, width, label='3sem2K18')
-# rects2 = ax.bar(x + width/2, percentages.EE2K18sem6, width, label='3sem2K19')
-
-
-# # Add some text for labels, title and custom x-axis tick labels, etc.
-# ax.set_ylabel('Scores')
-# ax.set_title('Scores by 3sem  & sem')
-# ax.set_xticks(x)
-# ax.set_xticklabels(labels)
-# ax.legend()
-
-# ax.bar_label(rects1, padding=3)
-# ax.bar_label(rects2, padding=3)
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, percentages.EE2K18sem3, width, label='3sem2K18')
+rects2 = ax.bar(x + width/2, percentages.EE2K18sem6, width, label='6sem2K18')
+# rects3 = ax.bar(x + width/2, percentages.EE2K18sem6, width, label='6sem2K18')
 
 
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('% of students')
+ax.set_xlabel('Grade Point')
+ax.set_title('Grade Point in 3sem  & 6sem')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
 
-# importing package
-import matplotlib.pyplot as plt
-  
-# create data
-x = percentages.CO2K18sem5
-y = percentages.CO2K18sem3
-  
-# plot lines
-plt.plot(x, y, label = "line 1")
-plt.plot(y, x, label = "line 2")
-plt.legend()
+
+
+ax.bar_label(rects1, padding=3)
+ax.bar_label(rects2, padding=3)
+# ax.bar_label(rects3, padding=3)
+
+fig.tight_layout()
+
 plt.show()
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+# importing package
+# import matplotlib.pyplot as plt
+  
+# # create data
+# x = percentages.CO2K18sem5
+# y = percentages.CO2K18sem3
+  
+# # plot lines
+# plt.plot(x, y, label = "line 1")
+# plt.plot(y, x, label = "line 2")
+# plt.legend()
+# plt.show()
